d752cf6d843978a3c66ac802a4b0e92ae0569c21
unknown <playmakernyomlo@gmail.com>
2021-07-23 11:45:04 +0700

    fix meta url

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,13 @@ export default function Home({quiz}) {
         <meta name="description" content="Tired with COVID-19 News? Get Covid Info with Fun now! Challenge yourself. See your future predictions, according to your knowledge of the plague. [of course it's just a joke]" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://debut.vercel.app" />
         <meta property="og:title" content="DeBut - Pandemi Gabut" />
         <meta property="og:description" content="Tired with COVID-19 News? Get Covid Info with Fun now! Challenge yourself. See your future predictions, according to your knowledge of the plague. [of course it's just a joke]" />
         <meta property="og:image" content="https://i.ibb.co/mFytzFs/Frame-30.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://debut.vercel.app" />
         <meta property="twitter:title" content="DeBut - Pandemi Gabut" />
         <meta property="twitter:description" content="Tired with COVID-19 News? Get Covid Info with Fun now! Challenge yourself. See your future predictions, according to your knowledge of the plague. [of course it's just a joke]" />
         <meta property="twitter:image" content="https://i.ibb.co/mFytzFs/Frame-30.png" />
