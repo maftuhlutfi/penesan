4023462a2a318915b44fbaad24b5c6546e051ba8
unknown <playmakernyomlo@gmail.com>
2021-07-30 10:01:32 +0700

    fix container padding top on mobile

--- components/shared/Container.js
+++ components/shared/Container.js
@@ -4,7 +4,7 @@ const Container = ({children, ...props}) => {
     const isHome = useRouter().pathname == '/'
 
     return (
-        <div className={`w-full min-h-screen bg-bg-purple pt-16 ${!isHome && 'pt-32 lg:pt-44'}`} {...props}>
+        <div className={`w-full min-h-screen bg-bg-purple ${!isHome && 'pt-32 lg:pt-44'}`} {...props}>
             {children}
         </div>
     );
