098d4b1e51edc8220454a58e523599d09773572c
unknown <playmakernyomlo@gmail.com>
2021-08-05 12:22:32 +0700

    add user info, play again func, share func  in result page

--- components/shared/ShareModal/index.js
+++ components/shared/ShareModal/index.js
@@ -11,7 +11,7 @@ const ShareModal = ({onCancel, show, text}) => {
 
     return (
         <div className={`w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-75 z-30 flex items-center justify-center ${show ? 'block' : 'hidden'}`}>
-            <div className='bg-white flex flex-col justify-center items-center rounded-xl p-8'>
+            <div className='bg-white flex flex-col justify-center items-center rounded-xl p-8 z-10'>
                 <div className='flex gap-6 text-2xl lg:text-3xl text-white mb-8'>
                     {socmedList.map((item, index) => 
                         <a href={item.shareLink(location, text)} key={index} className={`w-10 h-10 rounded-md lg:w-14 lg:h-14 lg:rounded-xl flex justify-center items-center ${item.bg}`}>
--- pages/result/[id].js
+++ pages/result/[id].js
@@ -5,39 +5,53 @@ import Container from "../../components/shared/Container";
 import Image from 'next/image'
 import { urlFor } from "../../imageUrlBuilder";
 import Button from "../../components/shared/Button";
+import CustomHead from "../../components/shared/CustomHead";
+import { useState } from "react";
+import ShareModal from "../../components/shared/ShareModal";
 
-const ResultPage = ({result, score}) => {
+const ResultPage = ({result, score, user, quiz}) => {
     const router = useRouter()
+
+    const [showShareModal, setShowShareModal] = useState(false)
 
     if (router.isFallback) {
         return <Container>
             <p>Loading...</p>
         </Container>
     }
-    console.log(result.title)
 
     return (
         <>
-            <Container>
+            <CustomHead 
+                title={`${user.name}'s Result - Quiz ${quiz.title}`}
+                description={`${user.name}'s score is ${score}. ${result.title.replace(/{name}/g, user.name || 'You')}`}
+                url={`https://debut.vercel.app/quiz/${quiz.slug.current}`}
+            />
+            <Container style='pb-20'>
                 <div className='text-center w-full max-w-screen-md relative mx-auto leading-normal px-8 flex flex-col items-center'>
                     <h1 className='font-bold text-2xl lg:text-3xl mb-2 lg:mb-4'>You've Completed the Quiz</h1>
-                    <p className='text-text-secondary'>Your score is <b>{score.toFixed(0)}</b> & here is your result:</p>
+                    <p className='text-text-secondary'>{user.name + "'s" || 'Your'} score is <b>{score}</b> & here is your result:</p>
                     <div className='w-64 h-56  relative rounded-xl overflow-hidden my-8'>
                         <Image src={urlFor(result.image).url()} layout='fill' className='object-cover object-center' />
                     </div>
-                    <p className='italic font-semibold bg-white p-4 rounded-xl border-2 border-gray-700 shadow-quiz-card md:mb-4'>" {result.title.replace(/{name}/g, 'Enggar')} "</p>
+                    <p className='italic font-semibold bg-white p-4 rounded-xl border-2 border-gray-700 shadow-quiz-card md:mb-4'>" {result.title.replace(/{name}/g, user.name || 'You')} "</p>
                     <div className='flex gap-4 mt-6 items-center fixed bottom-0 bg-white justify-center z-10 w-full left-0 pt-4 pb-8 md:static md:bg-transparent md:p-0'>
-                        <Button variant='primary'>
+                        <Button variant='primary' onClick={() => router.push(`/quiz/${quiz.slug.current}/do`)}>
                             <i className='icon-rotate mr-3' />
                             Play Again
                         </Button>
-                        <Button variant='secondary'>
+                        <Button variant='secondary' onClick={() => setShowShareModal(true)}>
                             <i className='icon-share mr-3' />
                             Share
                         </Button>
                     </div>
                 </div>
             </Container>
+            <ShareModal 
+                show={showShareModal}
+                onCancel={() => setShowShareModal(false)}
+                text={`${user.name}'s score is ${score}`}
+            />
         </>
     );
 }
@@ -52,7 +66,12 @@ const resultQuery = groq`
     *[_type == "result" && _id == $id][0] {
         score,
         quiz->{
-            scoring
+            title,
+            scoring,
+            slug
+        },
+        user->{
+            name
         }
     }
 `
@@ -62,12 +81,16 @@ export async function getStaticProps({params}) {
         id: params.id
     })
     const scoring = await res.quiz.scoring.sort((a, b) => a.min <= b.min ? 1 : -1)
-    const score = await res.score
+    const score = await res.score.toFixed(0)
+    const user = await res.user
+    const quiz = await res.quiz
 
     return {
         props: {
             result: scoring.filter(s => s.min <= score)[0],
-            score
+            score,
+            user,
+            quiz
         },
     }
 }
