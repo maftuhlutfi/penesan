8190cd8e53f7b68287788fab33f051defcf259e7
unknown <playmakernyomlo@gmail.com>
2021-08-06 19:31:53 +0700

    remove env local

--- components/LoginSignUp/LoginWithSocialBtn.js
+++ components/LoginSignUp/LoginWithSocialBtn.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const LoginWithSocialBtn = ({icon, label, provider, callbackUrl}) => {
     return (
-        <button className='w-full flex items-center shadow appearance-none border rounded px-4 py-2 text-sm hover:bg-more-light-purple' onClick={() => signIn(provider, {callbackUrl})}>
+        <button className='w-full flex items-center shadow appearance-none border rounded px-4 py-2 text-sm hover:bg-more-light-purple' onClick={() => signIn(provider, {callbackUrl: callbackUrl})}>
             <div className='h-4 w-4 relative mr-3'>
                 <Image src={`/social-icons/${icon}.svg`} className='object-center object-cover' layout='fill' />
             </div>
