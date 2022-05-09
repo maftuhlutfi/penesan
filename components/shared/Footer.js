e791664160e57a0671aadf1f22c8ea2f713edf2e
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-10 06:40:01 +0700

    logo with language changes

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useContext } from "react";
+import { LanguageContext } from "../Context";
 import menuList from "./menuList";
 
 const Footer = () => {
+    const { lang } = useContext(LanguageContext)
+
     return (
         <div className='flex flex-col w-full gap-10 px-8 py-16 text-center bg-text-primary md:px-20 lg:flex-row lg:justify-between lg:text-left'>
             <div className='flex flex-col items-center lg:items-start lg:w-1/4'>
                 <Link href='/' >
                     <a>
-                        <Image src='/logo/logo-white-text.svg' width={90} height={45} alt='logo-penesan-white' />
+                        <Image src={lang == 'bug' ? '/logo/logo-white-text.svg' : '/logo/logo-latin-white-text.svg'} width={156} height={45} alt='logo-penesan-white' />
                     </a>
                 </Link>
-                <p className='mt-4 text-sm leading-normal text-gray-400 lg:mt-1 lg:text-base'>Jl. Colombo Yogyakarta No.1, Karang Malang, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
+                <p className='mt-4 text-sm leading-normal text-gray-400 lg:mt-2 lg:text-base'>Jl. Colombo Yogyakarta No.1, Karang Malang, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
             </div>
             <div className='flex flex-col items-center lg:items-start'>
                 <h1 className='text-xl font-bold text-white lg:text-2xl'>Menu</h1>
--- components/shared/Logo.js
+++ components/shared/Logo.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useContext } from "react";
+import { LanguageContext } from "../Context";
 
 const Logo = ({ dark, width, height }) => {
+    const { lang } = useContext(LanguageContext)
+
     return (
         <Link href='/' >
             <a>
-                <Image src='/logo/logo.svg' width={width || height * 2} height={height} alt='logo-debut' />
+                <Image src={lang == 'bug' ? '/logo/logo.svg' : '/logo/logo-latin.svg'} width={width || lang == 'bug' ? height * 2 : height * 2.3} height={height} alt='logo-debut' />
             </a>
         </Link>
     );
