a6c9d2b437692b69f26345b1b6e1a4b093c2197c
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-10 16:26:53 +0700

    quiz card translation

--- components/Explore/QuizItem.js
+++ components/Explore/QuizItem.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { urlFor } from "../../imageUrlBuilder";
+import { useContext } from "react";
+import { LanguageContext } from "../Context";
+
 
 const QuizItem = ({ title, slug, mainImage, categories }) => {
+    const isIndo = useContext(LanguageContext).lang == 'indo'
+
     return (
         <Link href={`/quiz/${slug}`}>
             <div className={`w-full group h-80 lg:h-96 relative rounded-2xl overflow-hidden shadow-quiz-card border-2 cursor-pointer border-gray-800 
                 transform hover:translate-y-3 hover:shadow-none lg:rounded-3xl transition-all ease-in duration-200`}>
                 <Image src={urlFor(mainImage).url()} layout='fill' className='object-cover object-center' alt={`${title} image`} />
-                <h1 className='absolute bottom-0 w-full p-4 text-xl font-bold transition-all duration-300 ease-in transform bg-white lg:text-2xl lg:p-8'>{title}</h1>
+                <h1 className='absolute bottom-0 w-full p-4 text-xl font-bold transition-all duration-300 ease-in transform bg-white lg:text-2xl lg:p-8'>{isIndo ? title.split(" || ")[0] : title.split(" || ")[1]}</h1>
                 <div className='absolute flex gap-2 text-sm font-medium top-4 right-4'>
-                    {categories.map((category, index) => <p className='p-2 bg-white border-2 border-gray-700 rounded-lg' key={index}>{category.title}</p>)}
+                    {categories.map((category, index) => <p className='p-2 bg-white border-2 border-gray-700 rounded-lg' key={index}>{isIndo ? category.title.split(" || ")[0] : category.title.split(" || ")[1]}</p>)}
                 </div>
             </div>
         </Link>
--- components/Home/QuizItem.js
+++ components/Home/QuizItem.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { urlFor } from "../../imageUrlBuilder";
+import { useContext } from "react";
+import { LanguageContext } from "../Context";
+
 
 const QuizItem = ({ slug, mainImage, index, title }) => {
+    const isIndo = useContext(LanguageContext).lang == 'indo'
+
     return (
         <Link key={index} href={`/quiz/${slug}`}>
             <div className={`w-full group h-80 relative rounded-2xl overflow-hidden shadow-quiz-card border-2 cursor-pointer border-gray-800 
                 transform hover:translate-y-3 hover:shadow-none lg:rounded-3xl transition-all ease-in duration-200 ${index == 0 ? 'lg:row-span-2 lg:h-full' : 'lg:h-64'}`}>
                 <Image src={urlFor(mainImage).url()} layout='fill' className='object-cover object-center' alt={`${title} image`} />
-                <h1 className='absolute bottom-0 w-full p-4 text-xl font-bold transition-all duration-300 ease-in transform bg-white lg:text-2xl lg:p-8'>{title}</h1>
+                <h1 className='absolute bottom-0 w-full p-4 text-xl font-bold transition-all duration-300 ease-in transform bg-white lg:text-2xl lg:p-8'>
+                    {isIndo ? title.split(" || ")[0] : title.split(" || ")[1]}
+                </h1>
             </div>
         </Link>
     );
