f0c9bd37fb9b99bb52b333ff1d06b4b9a8ffbd9a
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-10 16:44:43 +0700

    footer translation

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -1,35 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useContext } from "react";
+import useTranslations from "../../hooks/useTranslations";
 import { LanguageContext } from "../Context";
 import menuList from "./menuList";
 
 const Footer = () => {
-    const { lang } = useContext(LanguageContext)
+    const isIndo = useContext(LanguageContext).lang == 'indo'
+    const text = useTranslations('footer')
 
     return (
         <div className='flex flex-col w-full gap-10 px-8 py-16 text-center bg-text-primary md:px-20 lg:flex-row lg:justify-between lg:text-left'>
             <div className='flex flex-col items-center lg:items-start lg:w-1/4'>
                 <Link href='/' >
                     <a>
-                        <Image src={lang == 'bug' ? '/logo/logo-white-text.svg' : '/logo/logo-latin-white-text.svg'} width={156} height={45} alt='logo-penesan-white' />
+                        <Image src={isIndo ? '/logo/logo-latin-white-text.svg' : '/logo/logo-white-text.svg'} width={156} height={45} alt='logo-penesan-white' />
                     </a>
                 </Link>
-                <p className='mt-4 text-sm leading-normal text-gray-400 lg:mt-2 lg:text-base'>Jl. Colombo Yogyakarta No.1, Karang Malang, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
+                <p className='mt-4 text-sm leading-normal text-gray-400 lg:mt-2 lg:text-base'>{text[0][0]}</p>
             </div>
             <div className='flex flex-col items-center lg:items-start'>
-                <h1 className='text-xl font-bold text-white lg:text-2xl'>Menu</h1>
+                <h1 className='text-xl font-bold text-white lg:text-2xl'>{text[1][0]}</h1>
                 <div className='flex justify-center w-full gap-6 mt-4 text-sm leading-normal text-gray-400 lg:text-base'>
-                    {[...menuList, { name: 'Login', href: '/login' }].map((item, index) =>
+                    {[...menuList, { name: 'Masuk || ᨆᨔᨘᨀᨛ', href: '/login' }].map((item, index) =>
                         <Link href={item.href} key={index}>
-                            {item.name}
+                            {isIndo ? item.name.split(" || ")[0] : item.name.split(" || ")[1]}
                         </Link>
                     )}
                 </div>
             </div>
             <div className='flex flex-col items-center lg:items-start lg:w-1/4'>
-                <h1 className='text-xl font-bold text-white lg:text-2xl'>Disclaimer</h1>
-                <p className='mt-4 text-sm leading-normal text-gray-400 lg:text-base'>Semua konten di website ini diperoleh dari beberapa sumber dan digunakan untuk pembelajaran/hiburan.</p>
+                <h1 className='text-xl font-bold text-white lg:text-2xl'>{text[2][0]}</h1>
+                <p className='mt-4 text-sm leading-normal text-gray-400 lg:text-base'>{text[2][1]}</p>
             </div>
         </div>
     );
