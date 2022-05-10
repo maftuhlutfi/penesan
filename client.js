0c3a4521daab819bd7511ae29122335f40c3ef1b
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-10 09:53:58 +0700

    change sanity project client

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: 'jc9hi5en', // you can find this in sanity.json
-    dataset: 'production', // or the name you chose in step 1
-    apiVersion: 'v1',
-    token: 'skae32ouh4jcYqrKuvqjIyK7HxnAdeXZrAXbPWIASM1LKxISnVkQ1D6wBBtwxahCixfpcFtR5XkSYSyZvVxceoNbXoY2VzyvCqYcUaovGTAQCL6382WNpA6rQ2ZIu5S3SrSfIsI6sipQ2hmfLHIbqWZjpoqnqpSy8cgXFR2nlrseQ0Tg7Brc',
-    useCdn: false // `false` if you want to ensure fresh data
-  })
+  projectId: '8dx4tpeh', // you can find this in sanity.json
+  dataset: 'test', // or the name you chose in step 1
+  apiVersion: 'v1',
+  token: 'skv6SKTmKYHykdxQmTT7knS5YBdE7X2XnyaAviJYraX4J1qlIxW36RTeC4d481nQKusJKklee0Ueu5PbbNSEtjeGqxKaYSPE8cyAl4FvyTwtL8sFv7yu2IoMBn895VAgZ3dQDM94bpd0WbGy7QqqCfimJ8hqoo0SfL5zw3ww7BWkWXsSvo3E',
+  useCdn: false // `false` if you want to ensure fresh data
+})
